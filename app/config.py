ec83b1593e9411f71ca12561aed6a96b2f6fcbe7
Gargamel19 <Hinter1baumesRindeGH>
2021-06-16 13:18:39 +0200

    messed up requirements.txt

--- app/config.py
+++ app/config.py
@@ -3,6 +3,8 @@ import os
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(BASEDIR, 'app.db')
+if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 SQLALCHEMY_MIGRATE_REPO = os.path.join(BASEDIR, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
