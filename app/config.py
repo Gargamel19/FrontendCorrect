63cb60db41565cecada2c323fd18ae435a12b398
Gargamel19 <Hinter1baumesRindeGH>
2021-06-16 12:31:06 +0200

    updated Database_url

--- app/config.py
+++ app/config.py
@@ -2,8 +2,7 @@ import os
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-SQLALCHEMY_DATABASE_URI = "postgres://uveifpynzjoosq:1d02813dfd2dfcdda3ef7c154f8047ad6a7a601aa1ae03e06954540495d4be24@ec2-54-228-139-34.eu-west-1.compute.amazonaws.com:5432/d4mujlupdais5d"
-#SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(BASEDIR, 'app.db')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(BASEDIR, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(BASEDIR, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
