69bf80f473492eefdc868ae4cbe77e4b2dbc5b45
Matthew Munson <matthew.munson@uni-hamburg.de>
2021-06-17 12:38:44 +0200

    Added .env loading to config file

--- app/config.py
+++ app/config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
