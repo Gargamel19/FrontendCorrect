b39141d5f061e0362720295f7ec564c3dbddb02a
Gargamel19 <Hinter1baumesRindeGH>
2021-06-30 20:13:23 +0200

    Added "Attributes" to the View, read the new dataformat AND Save The new dataformat

--- app/routes.py
+++ app/routes.py
@@ -63,11 +63,6 @@ def get_backup_list(name, text):
         new_backups.append([backup, str(dt)])
     return new_backups
 
-@app.route("/user")
-def ferdi():
-    add_user("FettarmQP", "trendelenburger19.04@googlemail.com", "Pa55wort", True)
-    return redirect(url_for('login'))
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -204,8 +199,17 @@ def sammlung_text(name, text):
     response = requests.get(backend_endpoint + "/sammlung/{}/text/{}".format(name, text))
     files = json.loads(response.text)
     return_list = []
+
     for form in files:
-        return_list.append([form[0], form[1]])
+        words = []
+        for word in form["words"]:
+            args = []
+            for key in word[1].keys():
+                args.append([key, word[1][key]])
+            words.append([word[0], args])
+
+        return_list.append([form["function"], words])
+
     return render_template('index_ueberlieferung_text.html', title='Text', files=return_list,
                            url=request.url, len=len(return_list), backups=get_backup_list(name, text), username=username)
 
@@ -247,11 +251,17 @@ def sammlung_text_post(name, text):
     erg = []
     for key in request.form.keys():
         if str(key).startswith("cat_"):
-            erg.append([[], request.form.get(key)])
+            erg.append({"function": request.form.get(key), "words": []})
         elif str(key).startswith("wordContainer_"):
             if len(erg) == 0:
-                erg.append([[], "nosegment"])
-            erg[len(erg) - 1][0].append(request.form.get(key))
+                erg.append({"function": "nosegment", "words": []})
+            erg[len(erg) - 1]["words"].append([request.form.get(key), {}])
+        elif str(key).startswith("attValue_"):
+            if len(erg) == 0:
+                erg.append({"function": "nosegment", "words": ["", {}]})
+            newkey = request.form.get("attKey_" + str(key).replace(str(key).split("_")[0] + "_", ""))
+            if newkey != "":
+                erg[len(erg) - 1]["words"][len(erg[len(erg) - 1]["words"]) - 1][1][newkey] = request.form.get(key)
     url = backend_endpoint + "/sammlung/{}/text/{}".format(name, text)
     requests.post(url, json=json.dumps(erg, ensure_ascii=False))
     return redirect(url_for("sammlung_text", name=name, text=text))
