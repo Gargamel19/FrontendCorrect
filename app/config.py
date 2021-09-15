661aa5086682aeead737b87afbd8045b6ba4e868
Gargamel19 <Hinter1baumesRindeGH>
2021-09-15 15:50:08 +0200

    check password changes and mail changes

--- app/config.py
+++ app/config.py
@@ -6,8 +6,6 @@ load_dotenv(os.path.join(basedir, '.env'))
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(BASEDIR, 'app.db')
-if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 SQLALCHEMY_MIGRATE_REPO = os.path.join(BASEDIR, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
@@ -18,11 +16,8 @@ MY_IP = os.environ.get('MY_IP') or "http://127.0.0.1:5000/"
 # BACKEND
 #BACKEND_IP = "https://be-correct.herokuapp.com/"
 BACKEND_IP = os.environ.get('BACKEND_IP') or "http://127.0.0.1"
-#BACKEND_PORT = 80
-if os.environ.get('BACKEND_PORT'):
-    BACKEND_PORT = int(os.environ.get('BACKEND_PORT'))
-else:
-    BACKEND_PORT = 5001
+
+BACKEND_PORT = int(os.environ.get('BACKEND_PORT') or 5001)
 
 START_PORT = 0
 
