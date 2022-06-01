0c9f46326ea437a9aeb3454b443a0c5e25989801
Matthew Munson <matthew.munson@uni-hamburg.de>
2022-06-01 12:07:52 +0200

    Added error logging

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_login import LoginManager
 import app.config as config
 from app.commands import create_tables, add_user, create_test_tables
+import os
+import logging
+from logging.handlers import RotatingFileHandler
 
 from app.extentions import db, login_manager
 
@@ -16,5 +19,17 @@ login.login_view = 'login'
 app.cli.add_command(create_tables)
 app.cli.add_command(create_test_tables)
 app.cli.add_command(add_user)
+
+os.makedirs('logs', exist_ok=True)
+file_handler = RotatingFileHandler('logs/FrontEndCorrect.log',
+                                   maxBytes=10240, backupCount=10)
+file_handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s '
+    '[in %(pathname)s:%(lineno)d]'))
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+
+app.logger.setLevel(logging.INFO)
+app.logger.info('FrontEndCorrect startup')
 
 from app import routes, models
